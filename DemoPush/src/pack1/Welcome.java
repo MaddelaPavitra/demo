ed7aa1b7e59dcd40780f18c69b50613908e0cb21
MaddelaPavitra <123794059+MaddelaPavitra@users.noreply.github.com>
2023-10-30 12:07:35 +0530

    third commit

--- DemoPush/src/pack1/Welcome.java
+++ DemoPush/src/pack1/Welcome.java
@@ -6,6 +6,7 @@ public class Welcome {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to gprec");
 		System.out.println("wecome to cse");
+		System.out.println("Welcome to ECS");
 
 	}
 
